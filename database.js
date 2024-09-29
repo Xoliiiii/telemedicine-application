01ccb9595775c7a9fc203810dbcf9463ccbf13e4
Xolisa Nunge <xolinunge0@gmail.com>
2024-09-29 19:34:25 +0200

    pulling results from the database and handling the dta to present itself as an array as the information is in table format

--- database.js
+++ database.js
@@ -13,3 +13,25 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL database!');
 });
+
+// executing query
+connection.query('SELECT * FROM patients',(results, err) => {
+    if (err) {
+        console.error('Error executing query:', err)
+        return;
+    }
+
+    console.log('Query Results:',results)
+
+
+    //HAndling results if its a array 
+
+    if (Array.isArray(results)){
+        results.forEach((row, index) => {
+            console.log(`Row ${index}, row`)
+        });
+    }
+
+    connection.end();
+});
+
